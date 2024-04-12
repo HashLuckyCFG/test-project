d3697c428a0af084f659cd8fc3c8068ebed0afe9
_Alchemist <ar4a100@gmail.com>
2024-04-12 15:17:36 +0300

    Some updates

--- src/components/TableApp.js
+++ src/components/TableApp.js
@@ -9,17 +9,17 @@ function TableApp() {
   useEffect(() => {
     const fetchData = async () => {
       const addresses = [
-        { address: '0xbAB31fed20b2862CB4d734C0FD04A79A3195bf90', number: 16}, 
-        { address: '0xdab4b1280f01C333a37f83fe9F785D70bB4367Af', number: 18},
-        { address: '0xdd168C5003e280eB70bB890FD3245FFC3e43E740', number: 19},
-        { address: '0x902E57be4b741570CeA27265517F90E90BDc9427', number: 20},
-        { address: '0xBE9FcdEa0Cd0eCD894F5bac603E702d6342637d0', number: 21},
-        { address: '0xEED4e59568BCc63597B6a1A8a6cE645eC581694E', number: 22},
-        { address: '0x6Dc86aA96D0827aB394DbdfD0089145F162F62A1', number: 23},
-        { address: '0xA326897BCDe01d6B9cEC65122b6B70d1DA16D818', number: 24},
-        { address: '0x0CB13166248BC7d29630D24B77187376096e2079', number: 25},
+        { address: '0xbAB31fed20b2862CB4d734C0FD04A79A3195bf90', number: 16 },
+        { address: '0xdab4b1280f01C333a37f83fe9F785D70bB4367Af', number: 18 },
+        { address: '0xdd168C5003e280eB70bB890FD3245FFC3e43E740', number: 19 },
+        { address: '0x902E57be4b741570CeA27265517F90E90BDc9427', number: 20 },
+        { address: '0xBE9FcdEa0Cd0eCD894F5bac603E702d6342637d0', number: 21 },
+        { address: '0xEED4e59568BCc63597B6a1A8a6cE645eC581694E', number: 22 },
+        { address: '0x6Dc86aA96D0827aB394DbdfD0089145F162F62A1', number: 23 },
+        { address: '0xA326897BCDe01d6B9cEC65122b6B70d1DA16D818', number: 24 },
+        { address: '0x0CB13166248BC7d29630D24B77187376096e2079', number: 25 },
       ];
-    
+
       // Создаем массив промисов для каждого запроса
       const promises = addresses.map((address, index) => {
         return new Promise((resolve, reject) => {
@@ -28,12 +28,12 @@ function TableApp() {
             // Выполняем запросы к API
             Promise.all([
               axios.get(`https://api-evm.orderly.org/v1/client/points?address=${address.address}`),
-              axios.get(`https://api-evm.orderly.org/v1/public/campaign/user?address=${address.address}&campaign_id=13`)
+              axios.get(`https://api-evm.orderly.org/v1/public/campaign/user?address=${address.address}&campaign_id=12`)
             ]).then(([pointsResponse, campaignResponse]) => {
               // Обрабатываем результаты запросов
               const pointsData = pointsResponse.data.data;
               const campaignData = campaignResponse.data.data;
-    
+
               // Вычисляем остальные данные и создаем объект с результатами
               const newDataItem = {
                 number: address.number,
@@ -46,7 +46,7 @@ function TableApp() {
                 accountPnL: (pointsData.current_epoch_points !== null && campaignData.total_deposit_amount !== null) ? (campaignData.end_account_value - campaignData.total_deposit_amount).toFixed(2) : null,
                 costPer1k: (pointsData.current_epoch_points !== null && campaignData.volume !== null && campaignData.volume !== 0) ? ((campaignData.end_account_value - campaignData.total_deposit_amount) / campaignData.volume * 1000).toFixed(2) : null
               };
-    
+
               // Разрешаем промис с объектом данных
               resolve(newDataItem);
             }).catch(error => {
@@ -54,16 +54,16 @@ function TableApp() {
               console.error('Error fetching data:', error);
               reject(error);
             });
-          }, index * 1000); // Устанавливаем задержку в 1 секунду перед каждым запросом
+          }, index * 500); // Устанавливаем задержку в 1 секунду перед каждым запросом
         });
       });
-    
+
       // Дожидаемся выполнения всех промисов
       Promise.all(promises)
         .then(newData => {
           // Устанавливаем новые данные
           setData(newData);
-          
+
           // Вычисляем среднее значение затрат на 1к оборота
           const averageCostPer1kValue = newData.reduce((acc, item) => {
             if (item.costPer1k !== null) {
@@ -71,7 +71,7 @@ function TableApp() {
             }
             return acc;
           }, 0) / newData.length;
-          
+
           // Устанавливаем среднее значение затрат на 1к оборота
           setAverageCostPer1k(averageCostPer1kValue.toFixed(2));
         })
@@ -80,14 +80,13 @@ function TableApp() {
           console.error('Error fetching data:', error);
         });
     };
-    
 
     fetchData();
   }, []);
 
   return (
     <div>
-      <h1>Orderly (LogX) Accounts Table</h1>
+      <h1>Orderly Accounts Table</h1>
       <table>
         <thead>
           <tr>
