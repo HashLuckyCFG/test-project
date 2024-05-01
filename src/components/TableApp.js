9c8474062940616795f88fe495fc4d6b950ce8b1
_Alchemist <ar4a100@gmail.com>
2024-05-01 10:07:01 +0300

    Some updates

--- src/components/TableApp.js
+++ src/components/TableApp.js
@@ -5,9 +5,43 @@ import '../css/Table.css';
 function TableApp() {
   const [data, setData] = useState([]);
   const [averageCostPer1k, setAverageCostPer1k] = useState(null);
+  const [currentEpochId, setCurrentEpochId] = useState(null);
+  const [totalTraders, setTotalTraders] = useState(null);
+  const [timeRemainingCurrentEpoch, setTimeRemainingCurrentEpoch] = useState(null);
+  const [timeRemainingLastEpoch, setTimeRemainingLastEpoch] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
+      // Получаем текущую эпоху
+      axios.get('https://api-evm.orderly.org/v1/public/points/epoch')
+        .then(response => {
+          setCurrentEpochId(response.data.data.current_epoch_id);
+          setTotalTraders(response.data.data.total_traders);
+          // Получаем данные всех эпох
+          return axios.get('https://api-evm.orderly.org/v1/public/points/epoch_dates');
+        })
+        .then(response => {
+          const epochData = response.data.data;
+
+          // Находим данные текущей эпохи
+          const currentEpoch = epochData.rows.find(epoch => epoch.epoch_id === currentEpochId);
+          const timestamp = Date.now();
+
+          // Вычисляем оставшееся время до конца текущей эпохи
+          const timeRemainingCurrent = currentEpoch.epoch_end_date - timestamp;
+          setTimeRemainingCurrentEpoch(timeRemainingCurrent);
+
+          // Находим данные последней эпохи
+          const lastEpoch = epochData.rows[epochData.rows.length - 1];
+
+          // Вычисляем оставшееся время до конца последней эпохи
+          const timeRemainingLast = lastEpoch.epoch_end_date - timestamp;
+          setTimeRemainingLastEpoch(timeRemainingLast);
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
+
       const addresses = [
         { address: '0xbAB31fed20b2862CB4d734C0FD04A79A3195bf90', number: 16 },
         { address: '0xdab4b1280f01C333a37f83fe9F785D70bB4367Af', number: 18 },
@@ -20,69 +54,65 @@ function TableApp() {
         { address: '0x0CB13166248BC7d29630D24B77187376096e2079', number: 25 },
       ];
 
-      // Создаем массив промисов для каждого запроса
       const promises = addresses.map((address, index) => {
         return new Promise((resolve, reject) => {
-          // Добавляем задержку в 1 секунду перед каждым запросом
           setTimeout(() => {
-            // Выполняем запросы к API
             Promise.all([
               axios.get(`https://api-evm.orderly.org/v1/client/points?address=${address.address}`),
               axios.get(`https://api-evm.orderly.org/v1/public/campaign/user?address=${address.address}&campaign_id=12`)
-            ]).then(([pointsResponse, campaignResponse]) => {
-              // Обрабатываем результаты запросов
-              const pointsData = pointsResponse.data.data;
-              const campaignData = campaignResponse.data.data;
-
-              // Вычисляем остальные данные и создаем объект с результатами
-              const newDataItem = {
-                number: address.number,
-                address: address.address,
-                points: pointsData.current_epoch_points,
-                rank: pointsData.current_epoch_rank,
-                userTotalVolume: campaignData.volume !== null ? campaignData.volume.toFixed(2) : null,
-                totalDeposit: campaignData.total_deposit_amount !== null ? campaignData.total_deposit_amount.toFixed(2) : null,
-                endAccountValue: campaignData.end_account_value !== null ? campaignData.end_account_value.toFixed(2) : null,
-                accountPnL: (pointsData.current_epoch_points !== null && campaignData.total_deposit_amount !== null) ? (campaignData.end_account_value - campaignData.total_deposit_amount).toFixed(2) : null,
-                costPer1k: (pointsData.current_epoch_points !== null && campaignData.volume !== null && campaignData.volume !== 0) ? ((campaignData.end_account_value - campaignData.total_deposit_amount) / campaignData.volume * 1000).toFixed(2) : null
-              };
-
-              // Разрешаем промис с объектом данных
-              resolve(newDataItem);
-            }).catch(error => {
-              // Обрабатываем ошибки запросов и отклоняем промис
-              console.error('Error fetching data:', error);
-              reject(error);
-            });
-          }, index * 500); // Устанавливаем задержку в 1 секунду перед каждым запросом
+            ])
+              .then(([pointsResponse, campaignResponse]) => {
+                const pointsData = pointsResponse.data.data;
+                const campaignData = campaignResponse.data.data;
+                const newDataItem = {
+                  number: address.number,
+                  address: address.address,
+                  totalPoints: pointsData.total_points,
+                  globalRank: pointsData.global_rank,
+                  points: pointsData.current_epoch_points,
+                  rank: pointsData.current_epoch_rank,
+                  userTotalVolume: campaignData.volume !== null ? campaignData.volume.toFixed(2) : null,
+                  totalDeposit: campaignData.total_deposit_amount !== null ? campaignData.total_deposit_amount.toFixed(2) : null,
+                  endAccountValue: campaignData.end_account_value !== null ? campaignData.end_account_value.toFixed(2) : null,
+                  accountPnL: (pointsData.current_epoch_points !== null && campaignData.total_deposit_amount !== null) ? (campaignData.end_account_value - campaignData.total_deposit_amount).toFixed(2) : null,
+                  costPer1k: (pointsData.current_epoch_points !== null && campaignData.volume !== null && campaignData.volume !== 0) ? ((campaignData.end_account_value - campaignData.total_deposit_amount) / campaignData.volume * 1000).toFixed(2) : null
+                };
+                resolve(newDataItem);
+              })
+              .catch(error => {
+                console.error('Error fetching data:', error);
+                reject(error);
+              });
+          }, index * 500);
         });
       });
 
-      // Дожидаемся выполнения всех промисов
       Promise.all(promises)
         .then(newData => {
-          // Устанавливаем новые данные
           setData(newData);
 
-          // Вычисляем среднее значение затрат на 1к оборота
           const averageCostPer1kValue = newData.reduce((acc, item) => {
             if (item.costPer1k !== null) {
               acc += parseFloat(item.costPer1k);
             }
             return acc;
           }, 0) / newData.length;
-
-          // Устанавливаем среднее значение затрат на 1к оборота
           setAverageCostPer1k(averageCostPer1kValue.toFixed(2));
         })
         .catch(error => {
-          // Обрабатываем ошибки
           console.error('Error fetching data:', error);
         });
     };
 
     fetchData();
-  }, []);
+  }, [currentEpochId]);
+
+  const formatTime = (milliseconds) => {
+    const days = Math.floor(milliseconds / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((milliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60));
+    return `${days} days: ${hours} hours: ${minutes} minutes`;
+  };
 
   return (
     <div>
@@ -92,6 +122,8 @@ function TableApp() {
           <tr>
             <th>Number</th>
             <th>Address</th>
+            <th>Total Points</th>
+            <th>Global Rank</th>
             <th>Current Epoch Points</th>
             <th>Current Epoch Rank</th>
             <th>User Total Volume</th>
@@ -106,6 +138,8 @@ function TableApp() {
             <tr key={index}>
               <td>{item.number}</td>
               <td>{item.address}</td>
+              <td>{item.totalPoints}</td>
+              <td>{item.globalRank}</td>
               <td>{item.points}</td>
               <td>{item.rank}</td>
               <td>{item.userTotalVolume}</td>
@@ -117,7 +151,10 @@ function TableApp() {
           ))}
         </tbody>
       </table>
-      <p>Average Cost per 1k: {averageCostPer1k !== null ? `$${averageCostPer1k}` : 'N/A'}</p>
+      <p>Average Cost per 1k: {averageCostPer1k !== null ? `$ ${averageCostPer1k}` : 'N/A'}</p>
+      <p>Total Traders: {totalTraders}</p>
+      <p>Time remaining in current epoch: {timeRemainingCurrentEpoch !== null ? formatTime(timeRemainingCurrentEpoch) : 'Loading...'}</p>
+      <p>Time remaining until end of last epoch: {timeRemainingLastEpoch !== null ? formatTime(timeRemainingLastEpoch) : 'Loading...'}</p>
     </div>
   );
 }
